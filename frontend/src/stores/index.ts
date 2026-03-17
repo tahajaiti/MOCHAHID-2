f595dcdaf51003bae9041a0c6aad088f83f6024c
tahajaiti <taha.jaiti@gmail.com>
2026-03-17 05:07:50 +0000

    feat(frontend): add index.ts

---
+++ frontend/src/stores/index.ts
@@ -0,0 +1,4 @@
+export { useAuthStore } from './authStore'
+export { useThemeStore } from './themeStore'
+export { useWatchlistStore } from './watchlistStore'
+export { useVideoStore } from './videoStore'
