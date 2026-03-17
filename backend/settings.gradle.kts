33dfcbacf00bdfad6d92be91d5b5531f33f03625
tahajaiti <taha.jaiti@gmail.com>
2026-03-17 05:07:40 +0000

    refac(backend): add settings.gradle.kts

---
+++ backend/settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "mochahid-backend"
+
+include("common")
+include("discovery-service")
+include("config-service")
+include("gateway-service")
+include("video-service")
+include("user-service")
