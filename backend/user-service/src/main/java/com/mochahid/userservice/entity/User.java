4d8cd85c784cf95856656b900cbf42b5ab51cfbe
tahajaiti <taha.jaiti@gmail.com>
2026-03-17 05:07:42 +0000

    feat(user-service): add User.java

---
+++ backend/user-service/src/main/java/com/mochahid/userservice/entity/User.java
@@ -0,0 +1,33 @@
+package com.mochahid.userservice.entity;
+
+import com.mochahid.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+}
