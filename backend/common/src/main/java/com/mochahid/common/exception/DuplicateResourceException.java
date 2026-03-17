98bd651185c1e429f2414ea78bf6ab7ac67a221d
tahajaiti <taha.jaiti@gmail.com>
2026-03-17 05:07:38 +0000

    feat(common): add DuplicateResourceException.java

---
+++ backend/common/src/main/java/com/mochahid/common/exception/DuplicateResourceException.java
@@ -0,0 +1,8 @@
+package com.mochahid.common.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}
