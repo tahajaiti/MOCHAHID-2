f174588473bab5d8be08a7e00a41deccc0698273
tahajaiti <taha.jaiti@gmail.com>
2026-03-17 05:07:44 +0000

    feat(video-service): add Category.java

---
+++ backend/video-service/src/main/java/com/mochahid/videoservice/entity/Category.java
@@ -0,0 +1,14 @@
+package com.mochahid.videoservice.entity;
+
+public enum Category {
+    ACTION,
+    COMEDY,
+    DRAMA,
+    SCIENCE_FICTION,
+    THRILLER,
+    HORROR,
+    DOCUMENTARY,
+    ROMANCE,
+    ANIMATION,
+    ADVENTURE
+}
